b15a62ca5a318ab732d54f04f3e71b84ecbf69aa
kylerzhang <kylerzhang11@gmail.com>
2013-03-21 19:49:08 +0800

    Add template.cc

---
+++ TopCoder/SRM/template.cc
@@ -0,0 +1,25 @@
+/*
+ * Author: Kai Zhang
+ * Email: kylerzhang11@gmail.com
+ */
+#include <stdio.h>
+
+#include <algorithm>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using std::list;
+using std::map;
+using std::pair;
+using std::set;
+using std::string;
+using std::vector;
+
+
+
+int main() {
+  return 0;
+}
